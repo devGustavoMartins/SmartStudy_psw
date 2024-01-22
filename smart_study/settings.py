b976bf5e0a1367086733b211d9d64825fffa726e
Gustavo <gustavo.om1206@gmail.com>
2024-01-22 00:28:27 -0300

    modified:   db.sqlite3 	new file:   requirements.txt 	modified:   smart_study/__pycache__/settings.cpython-311.pyc 	modified:   smart_study/__pycache__/wsgi.cpython-311.pyc 	modified:   smart_study/settings.py 	modified:   smart_study/wsgi.py 	new file:   vercel.json

--- smart_study/settings.py
+++ smart_study/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3hq09-%@c*b%f#-*m%^()hid0*gluw&vx-8d!8j2luf-+cvp+t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
